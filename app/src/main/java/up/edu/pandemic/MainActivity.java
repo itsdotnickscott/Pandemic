24aa4210aa008aace35ea8d1d1fdf488d5e5f23b
scottn22 <scottn22@up.edu>
2020-09-08 16:34:39 -0700

    Created Pandemic project.

---
+++ app/src/main/java/up/edu/pandemic/MainActivity.java
@@ -0,0 +1,14 @@
+package up.edu.pandemic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
