db983237aebf841485568607d1db47da157c265c
Nick <scottn22@up.edu>
2020-10-18 21:45:02 -0700

    GUI with map

--- app/src/main/java/up/edu/pandemic/MainActivity.java
+++ app/src/main/java/up/edu/pandemic/MainActivity.java
@@ -3,6 +3,8 @@ package up.edu.pandemic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
